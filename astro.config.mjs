e38579cd53553be1ed04d3351e32dd794df2c956
Hiroya Watanabe <n.notenki@gmail.com>
2026-04-03 15:53:45 +0900

    feat: サイトマップ追加・サイトURLをkattenihakodate.comに更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://katteni-hakodate.jp',
+  integrations: [sitemap()],
+  site: 'https://kattenihakodate.com',
 });
